d19cc024ec1255137398e97633d38634d894afc4
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2018-11-19 20:32:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,27 +11,5 @@ client.on('ready', () => {
 
 });
 
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 25,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
-
-message.author.send(`**مدة الرابط : يوم
- استخدامات الرابط : 25**`)
-
-       
        
 client.login(process.env.BOT_TOKEN);
