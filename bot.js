321615c7cb019964b3868489accd89a751e43643
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2018-12-01 11:40:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,6 +138,61 @@ message.author.send(`**مدة الرابط : يوم
 
 
 
+let points = JSON.parse(fs.readFileSync('./points.json', 'utf8'));
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+  };
+if (message.content.startsWith(prefix + 'فكك')) {
+    if(!message.channel.guild) return message.reply('**:no_entry: , هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+ 
+const type = require('./fkk.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**:timer: , لديك »15« ثانية فقط لتفكيك هذه الكلمة**').then(msg => {
+let embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setImage(`${item.type}`)
+msg.channel.send(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+         const sh = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setDescription('**:tada: , جيد , لقد حصلت على نقطة**')
+.addField('اكتب +نقاطي لرؤية نقاطك' , true)
+.setFooter(message.author.username, message.author.avatarURL)
+message.channel.sendEmbed(sh);
+        let won = collected.first().author;
+                points[won.id].points++;
+        })
+           .catch(collected => {
+            message.channel.send(`**:timer: , انتهئ الوقت , ولم يقم احد بتفكيك الكلمة**`);
+           })
+          fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+          })
+        })
+    })
+}
+})
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'نقاطي')) {
+    if(!message.channel.guild) return message.reply('**:no_entry: , هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+    let userData = points[message.author.id];
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+    .setColor('#000000')
+    .setDescription(`\`${userData.points}`\ : نقاطك`)
+    message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+});
+
+
 
 
 
