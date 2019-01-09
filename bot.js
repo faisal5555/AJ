93841349edfd356767dc31b90f70506c051b92b3
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2019-01-09 21:37:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,6 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`AJ Server`,"http://twitch.tv/idk")
 });
 
  
@@ -137,6 +136,116 @@ message.author.send(`**مدة الرابط : يوم
 
 
 
+
+
+const adminprefix = "A";;
+const devs = ['532625014382526467'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'playing')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`);
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then;
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`);
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'streaming')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير ستريم البوت إلى  ${argresult}**`);
+} else
+ if (message.content.startsWith(adminprefix + 'listening')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+    message.channel.sendMessage(`**تم تغيير ليسنق البوت إلى  ${argresult}**`);
+} else
+ if (message.content.startsWith(adminprefix + 'watching')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+    message.channel.sendMessage(`**تم تغيير واتشنق البوت إلى  ${argresult}**`);
+} 
+
+});
+
+
+
+
+
+client.on('message', message => {
+var prefix = "A";
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 410835593451405312)
+return;
+
+if (message.content.startsWith(prefix + 'dnd')) {
+  if (message.author.id !== '532625014382526467') return message.react('⚠')
+client.user.setStatus('dnd');  
+message.react("✅")
+}
+                        
+ });
+
+
+client.on('message', message => {
+var prefix = "A";
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 410835593451405312)
+return;
+
+
+if (message.content.startsWith(prefix + 'online')) {
+  if (message.author.id !== '532625014382526467') return message.react('⚠')
+  client.user.setStatus('online');  
+message.react("✅")
+}
+                        
+ });
+
+client.on('message', message => {
+var prefix = "A";
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 410835593451405312)
+return;
+if (message.content.startsWith(prefix + 'idle')) {
+   if (message.author.id !== '532625014382526467') return message.react('⚠')
+client.user.setStatus('idle');  
+message.react("✅")
+}
+                        
+ });
+
+
+client.on('message', message => {
+var prefix = "A";
+
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id == 410835593451405312)
+return;
+
+
+if (message.content.startsWith(prefix + 'invisible')) {
+    if (message.author.id !== '532625014382526467') return message.react('⚠')
+client.user.setStatus('invisible');  
+message.react("✔")
+}
+                        
+ });
 
 
 
